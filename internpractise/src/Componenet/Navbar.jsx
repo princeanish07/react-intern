bdb6f66dda83c6b8c1c96da60e9471196d070a81
princeanish07 <anishpathak456@gmail.com>
2024-05-09 10:28:56 +0545

    initial

--- internpractise/src/Componenet/Navbar.jsx
+++ internpractise/src/Componenet/Navbar.jsx
@@ -2,24 +2,29 @@ import {React,useState} from 'react'
 import { Link, animateScroll as scroll } from 'react-scroll';
 import './Css/Navbar.css';
 import logo from '.././assets/images/coverlogo.jpg';
+import menu_open from '../assets/images/menu_open.svg'
+import menu_close from '../assets/images/menu_close.svg'
+
 function Navbar() {
-  const [count, setCount] = useState(false);
-  const handleClick=()=>{
-    setCount(!count);
-  }
+  // const [count, setCount] = useState(false);
+  // const handleClick=()=>{
+  //   setCount(!count);
+  // }
 
   return (
     <div className='navbar'>
-      <img src={logo} alt="" />
-      <ul className="nav-menu">
-    
-      <li style={{color:count?'red':'inherit', cursor:'pointer'}} > <Link to='home' smooth={true} duration={1000} >  Home </Link> </li>
-      <li style={{color:count?'red':'inherit', cursor:'pointer'}} > <Link to='aboutus' smooth={true} duration={1000} >  About Us </Link> </li>
-        
-        <li style={{color:count?'red':'inherit', cursor:'pointer'}} > <Link to='Servicess' smooth={true} duration={1000} onClick={handleClick}>  Services </Link> </li>
-        <li style={{color:count?'red':'inherit', cursor:'pointer'}} > <Link to='work' smooth={true} duration={1000} > My Work </Link> </li>
 
-        <li style={{color:count?'red':'inherit', cursor:'pointer'}} > <Link to='contactus' smooth={true} duration={1000} onClick={handleClick}>  Contact </Link> </li>
+      <img src={logo} alt="" />
+      <img src={menu_open} alt="" className='nav-mob-open' />
+      <ul className="nav-menu">
+    <img src={menu_close} alt="" className='nav-mob-close' />
+      <li style={{ cursor:'pointer'}} > <Link to='home' smooth={true} duration={1000} >  Home </Link> </li>
+      <li style={{cursor:'pointer'}} > <Link to='aboutus' smooth={true} duration={1000} >  About Us </Link> </li>
+        
+        <li style={{ cursor:'pointer'}} > <Link to='Servicess' sm>  Services </Link> </li>
+        <li style={{ cursor:'pointer'}} > <Link to='work' smooth={true} duration={1000} > My Work </Link> </li>
+
+        <li style={{ cursor:'pointer'}} > <Link to='contactus' smooth={true} duration={1000} >  Contact </Link> </li>
 
       </ul>
       <div className="nav-connect">Connect With Me</div>

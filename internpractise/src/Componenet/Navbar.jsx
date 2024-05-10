de2d67cf9fdb0669b38ac7aec4e1373765e38572
princeanish07 <anishpathak456@gmail.com>
2024-05-10 10:53:13 +0545

    final commit

--- internpractise/src/Componenet/Navbar.jsx
+++ internpractise/src/Componenet/Navbar.jsx
@@ -1,4 +1,5 @@
 import {React,useState} from 'react'
+import { useRef } from 'react';
 import { Link, animateScroll as scroll } from 'react-scroll';
 import './Css/Navbar.css';
 import logo from '.././assets/images/coverlogo.jpg';
@@ -10,14 +11,24 @@ function Navbar() {
   // const handleClick=()=>{
   //   setCount(!count);
   // }
+  // const [menu, setmenu] = useState("navbar");
+  const menuRef =useRef();
+  const openMenu =()=>{
+    console.log('hello');
+    menuRef.current.style.right="0";
+  }
+  const closeMenu =()=>{
+    menuRef.current.style.right='-350px';
+  }
+
 
   return (
     <div className='navbar'>
 
       <img src={logo} alt="" />
-      <img src={menu_open} alt="" className='nav-mob-open' />
-      <ul className="nav-menu">
-    <img src={menu_close} alt="" className='nav-mob-close' />
+      <img src={menu_open} onClick={openMenu} alt="" className='nav-mob-open' />
+      <ul ref={menuRef} className="nav-menu">
+    <img src={menu_close}onClick={closeMenu} alt="" className='nav-mob-close' />
       <li style={{ cursor:'pointer'}} > <Link to='home' smooth={true} duration={1000} >  Home </Link> </li>
       <li style={{cursor:'pointer'}} > <Link to='aboutus' smooth={true} duration={1000} >  About Us </Link> </li>
         
